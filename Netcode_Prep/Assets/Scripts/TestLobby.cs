857ee85e4c219f30b8616b88b521516133f3fd4b
Nitronik16 <123427752+Nitronik16@users.noreply.github.com>
2025-03-26 03:08:48 -0700

    sync with game

---
+++ Netcode_Prep/Assets/Scripts/TestLobby.cs
@@ -0,0 +1,305 @@
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.VisualScripting;
+using System.Collections.Generic;
+
+public class TestLobby : MonoBehaviour
+{
+
+    /// <summary>
+    /// Tasks I need to do in order to sync with the game: 
+    /// Sync UI with player input variables
+    /// Construct player variables (data set objects within a dictionary) of Vic and Vlad 
+    /// </summary>
+
+    //get player input 
+    [HideInInspector] public string lobbyCode;
+    [HideInInspector] public bool isPrivate = true;
+    int playerNumber; 
+    private string playerName;
+
+    private Lobby hostLobby;
+    private Lobby joinedLobby; 
+    private float heartbeatTimer;
+    private float lobbyUpdateTimer;
+
+    public static Dictionary<string, PlayerDataObject> playerVariables = new Dictionary<string, PlayerDataObject>();
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CreateLobby();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ListLobbies();
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //JoinLobbyByCode();
+        }
+
+        HandleLobbyHeartbeat();
+        HandleLobbyPollForUpdates();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f)
+            {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    private async void HandleLobbyPollForUpdates()
+    {
+        if (joinedLobby != null)
+        {
+            lobbyUpdateTimer -= Time.deltaTime;
+            if (lobbyUpdateTimer < 0f)
+            {
+                float lobbyUpdateTimerMax = 1.1f;
+                lobbyUpdateTimer = lobbyUpdateTimerMax;
+
+                Lobby lobby = await LobbyService.Instance.GetLobbyAsync(joinedLobby.Id);
+                joinedLobby = lobby;
+            }
+        }
+    }
+
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "My lobby";
+            int maxPlayers = 2;
+            playerNumber = 1;
+
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions
+            {
+                IsPrivate = isPrivate,
+                Player = GetPlayer(),
+
+                ///Here, this is if we want to add a certain game mode in which 
+                ///the players should take into account when searching for a certain lobby
+                ///Since we are not doing any kind of stuff related to that, this is not needed.
+                ///I just thought to include it just in case or simply just to have it 
+                Data = new Dictionary<string, DataObject>
+                {
+                    { "Game Mode", new DataObject(DataObject.VisibilityOptions.Member, "Host Vic") }
+                }
+            };
+
+            Lobby lobby = await LobbyService.Instance.
+                CreateLobbyAsync(lobbyName, maxPlayers, createLobbyOptions);
+
+            hostLobby = lobby;
+            joinedLobby = hostLobby;
+            lobbyCode = hostLobby.LobbyCode;
+
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers + " " + lobby.LobbyCode);
+
+            PrintPlayers(hostLobby);
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void ListLobbies()
+    {
+        try {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+
+            };
+
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void JoinLobbyByCode(string lobbyCode)
+    {
+        try
+        {
+            playerNumber = 2;
+
+            JoinLobbyByCodeOptions joinLobbyCodeOptions = new JoinLobbyByCodeOptions
+            {
+                Player = GetPlayer()
+            };
+            Lobby lobby = await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode, joinLobbyCodeOptions);
+            joinedLobby = lobby;
+
+            Debug.Log("Joined Lobby with code " +  lobbyCode);
+
+            PrintPlayers(joinedLobby);
+
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private Player GetPlayer()
+    {
+        return new Player
+        {
+            Data = new Dictionary<string, PlayerDataObject>
+                    {
+                        { "PlayerName", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, "Player " + playerNumber)}
+                    }
+        };
+    }
+
+    private void PrintPlayers()
+    {
+        PrintPlayers(joinedLobby);
+    }
+
+    private void PrintPlayers(Lobby lobby)
+    {
+        foreach (Player player in lobby.Players)
+        {
+            Debug.Log(player.Id + " " + player.Data["PlayerName"].Value + " " + lobby.Data["GameMode"].Value);
+        }
+    }
+
+    private async void UpdateLobbyGamemode(string gameMode)
+    {
+        try
+        {
+            hostLobby = await LobbyService.Instance.UpdateLobbyAsync(hostLobby.Id, new UpdateLobbyOptions
+            {
+                Data = new Dictionary<string, DataObject>
+            {
+                { "GameMode", new DataObject(DataObject.VisibilityOptions.Member, gameMode)}
+
+            }
+            });
+
+            PrintPlayers(hostLobby);
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void UpdatePlayerName(string newPlayerName)
+    {
+        try
+        {
+            playerName = newPlayerName;
+            await LobbyService.Instance.UpdatePlayerAsync(joinedLobby.Id, AuthenticationService.Instance.PlayerId, new UpdatePlayerOptions
+            {
+                Data = new Dictionary<string, PlayerDataObject>
+            {
+                { "PlayerName", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, "Player " + playerNumber)}
+            }
+            });
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void LeaveLobby()
+    {
+        try
+        {
+            await LobbyService.Instance.RemovePlayerAsync(joinedLobby.Id, AuthenticationService.Instance.PlayerId);
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    /// <summary>
+    /// Remember to sync the player id intake via the click of a button connected to the player in the 
+    /// player lobby UI. 
+    /// </summary>
+    private async void KickPlayer()
+    {
+        try
+        {
+            await LobbyService.Instance.RemovePlayerAsync(joinedLobby.Id, joinedLobby.Players[1].Id);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void MigrateLobbyHost()
+    {
+        try
+        {
+            hostLobby = await LobbyService.Instance.UpdateLobbyAsync(hostLobby.Id, new UpdateLobbyOptions
+            {
+                HostId = joinedLobby.Players[1].Id
+            });
+            joinedLobby = hostLobby;
+
+            PrintPlayers(hostLobby);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void DeleteLobby()
+    {
+        try
+        {
+            await LobbyService.Instance.DeleteLobbyAsync(joinedLobby.Id);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+}
